e7fa08aef1df5d08b501c87f23c64c7f415a08ef
Lopacinski <augustin.lopacinski@epitech.eu>
2017-03-04 01:03:29 +0100

    merge

--- src/extract_data.c
+++ src/extract_data.c
@@ -1,4 +1,3 @@
-
 /*
 ** extract_data.c for my_tar in /home/esceve/EPITECH/CPE/CPE_2016_my_tar/src
 **
@@ -6,7 +5,7 @@
 ** Login   <esceve@epitech.net>
 **
 ** Started on  Fri Mar  3 23:50:21 2017
-** Last update Fri Mar  3 23:50:21 2017
+** Last update Sat Mar  4 00:49:33 2017 Augustin Lopacinski
 */
 
 #include "../include/include.h"
@@ -25,14 +24,15 @@ t_file_content		*extract_data(char **file_content, int count)
   int fd;
   t_file_content *data_file;
   struct stat filestat;
-  int toto;
-
-  i = 1;
+  int test;
+  
+  i = 2;
   fd = 0;
   data_file = malloc(sizeof(t_file_content) * (count - 1));
   while (i != count)
     {
-      // fd = open(file_content[i], O_RDONLY);
+      test = open(file_content[i], O_RDONLY);
+      error_extract(test);
       stat(file_content[i], &filestat);
       printf("filecontent[%d] : %s\n", i, file_content[i] );
       printf("filesize : %lu\n", filestat.st_size);
